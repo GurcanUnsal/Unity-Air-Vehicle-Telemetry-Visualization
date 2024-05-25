b92c7a3a025258c0fff247a781878cf2cf280f0a
Gurcan-Unsal <gurcanunsal@outlook.com>
2024-05-25 12:47:17 +0300

    added file explorer and csv reader

--- Assets/Scripts/FileUploadScripts/FileManager.cs
+++ Assets/Scripts/FileUploadScripts/FileManager.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
+using System.IO;
 
 public class FileManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    string path;
+    public string firstRow;
 
-    // Update is called once per frame
-    void Update()
+    public void OpenFileBrowser()
     {
-        
+        path = EditorUtility.OpenFilePanel("CSV File Reader", "", "csv");  //Read the dataset file path
+		ReadDataset();
+	}
+
+    private void ReadDataset()
+    {
+        if (path != null)
+        {
+            StreamReader streamReader = new StreamReader(path);  //Read the dataset with the file path
+            bool endOfFile = false;
+
+            while (!endOfFile)
+            {
+                string stringData = streamReader.ReadLine();
+                if (stringData == null)
+                {
+                    endOfFile = true;
+                    break;
+                }
+                var dataValues = stringData.Split(',');
+
+                for (int i = 0; i < dataValues.Length; i++)
+                {
+                    Debug.Log("Value: " + i.ToString() + " " + dataValues[i].ToString());
+                }
+            }
+        }
+
+
     }
 }
